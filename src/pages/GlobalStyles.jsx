befc9ee272a7adc9b828fcb5e71a4e062ba509af
hahee <hahee_hi@naver.com>
2026-05-13 00:11:53 +0900

    fonts

--- src/pages/GlobalStyles.jsx
+++ src/pages/GlobalStyles.jsx
@@ -1,42 +1,21 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
- 
-@font-face {
-  font-family: 'Pretendard';
-  src: url('/fonts/pretendard/Pretendard-Regular.woff2') format('woff2');
-  font-weight: 400;
-  font-display: swap;
-}
+  @font-face {
+    font-family: 'Pretendard Variable';
+    src: url('/fonts/pretendard/PretendardVariable.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+    font-display: block;
+  }
 
-@font-face {
-  font-family: 'Pretendard';
-  src: url('/fonts/pretendard/Pretendard-Medium.woff2') format('woff2');
-  font-weight: 500;
-  font-display: swap;
-}
-
-@font-face {
-  font-family: 'Pretendard';
-  src: url('/fonts/pretendard/Pretendard-SemiBold.woff2') format('woff2');
-  font-weight: 600;
-  font-display: swap;
-}
-
-@font-face {
-  font-family: 'Pretendard';
-  src: url('/fonts/pretendard/Pretendard-Bold.woff2') format('woff2');
-  font-weight: 700;
-  font-display: swap;
-}
-  
-@font-face {
-  font-family: 'DNFBitBitv2';
-  src: url('/fonts/DNFBitBitv2/DNFBitBitv2.ttf') format('truetype');
-  font-weight: normal;
-  font-style: normal;
-  font-display: swap;
-}
+  @font-face {
+    font-family: 'DNFBitBitv2';
+    src: url('/fonts/DNFBitBitv2/DNFBitBitv2.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+    font-display: block;
+  }
 
   * {
     margin: 0;
@@ -45,14 +24,13 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background-color: #f0f0f0; /* 원하는 배경색 */
+    background-color: #f0f0f0;
     margin: 0;
     padding: 0;
     width: 100%;
     height: 100%;
-    overflow-x: hidden; /* 가로 스크롤 방지 */
+    overflow-x: hidden;
     font-family: 'Pretendard Variable', sans-serif;
-  font-family: 'DNFBitBitv2', sans-serif;
   }
 
   #root {
